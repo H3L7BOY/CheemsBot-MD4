e8cfd0c7129ba74f82e836944bfb6237fa2bdef0
H3L7BOY <109512295+H3L7BOY@users.noreply.github.com>
2022-07-30 11:43:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -35,7 +35,7 @@ global.autoRecord = false //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
-global.vidmenu = fs.readFileSync("https://i.imgur.com/k2dnOWd.mp4") //gif and video menu
+global.vidmenu = fs.readFileSync("./XeonMedia/theme/Cheems-bot.mp4") //gif and video menu
 
 //documents variants
 global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
